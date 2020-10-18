35825f2e0aee5789b5af391c3966d1dc4ad90904
Emanual Jade <emanualjade@gmail.com>
2020-10-18 15:35:04 -0700

    Add new welcome

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console')
+const pkgJSON = require('./package.json')
+const welcome = require('cli-welcome')
 
-clearConsole()
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Howdy, nice to meet ya!`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true
+})
 
 console.log(`
 Emanual Jade - Software Engineer
