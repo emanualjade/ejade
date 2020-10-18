607e63d1e4efd5bf1b23832a0c57be499e911815
Emanual Jade <emanualjade@gmail.com>
2020-10-18 16:45:35 -0700

    Add colors

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
-const pkgJSON = require('./package.json')
-const welcome = require('cli-welcome')
+const pkgJSON = require("./package.json");
+const welcome = require("cli-welcome");
+const chalk = require("chalk");
+
+const log = console.log;
+const twitterColor = chalk.hex(`#1da1f2`).bold.inverse
+const githubColor = chalk.hex(`#6cc644`).bold.inverse
+const brandColor = chalk.hex(`#6937ff`).bold.inverse
+const dim = chalk.dim
+const italic = chalk.italic
 
 welcome({
-  title: pkgJSON.name,
+  title: "Emanual Jade",
   tagLine: `Howdy, nice to meet ya!`,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: `#FADC00`,
+  bgColor: `#6937ff`,
   color: `#000000`,
   bold: true,
-  clear: true
-})
+  clear: true,
+});
 
-console.log(`
-Emanual Jade - Software Engineer
+log(`
+${italic(`Software Engineer`)}
 
-  🐦 - Twitter: emanualjade
-  📖 - Github: emanualjade
-  😎 - Blog: cracklehat
+${twitterColor(` Twitter `)} ${dim(`emanualjade`)}
+${githubColor(` Github `)} ${dim(`emanualjade`)}
+${brandColor(` Blog `)} ${dim(`cracklehat`)}
 `);
