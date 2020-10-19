ea875270a8a666918b924b8bdaa0eb77545fe37b
Emanual Jade <emanualjade@gmail.com>
2020-10-18 17:04:51 -0700

    Add some log utils with color

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const pkgJSON = require("./package.json");
 const welcome = require("cli-welcome");
 const chalk = require("chalk");
+const sym = require('log-symbols')
 
 const log = console.log;
 const twitterColor = chalk.hex(`#1da1f2`).bold.inverse
@@ -9,6 +10,12 @@ const githubColor = chalk.hex(`#6cc644`).bold.inverse
 const brandColor = chalk.hex(`#6937ff`).bold.inverse
 const dim = chalk.dim
 const italic = chalk.italic
+
+// Alerts
+const success = chalk.green
+const info = chalk.blue
+const warning = chalk.keyword('orange')
+const error = chalk.red.bold
 
 welcome({
   title: "Emanual Jade",
@@ -28,3 +35,13 @@ ${twitterColor(` Twitter `)} ${dim(`emanualjade`)}
 ${githubColor(` Github `)} ${dim(`emanualjade`)}
 ${brandColor(` Blog `)} ${dim(`cracklehat`)}
 `);
+
+console.log(`
+  ${sym.success} ${success(` SUCCESS `)} Thanks for checking out my CLI.
+
+  ${sym.info} ${info(` INFO `)} Playing with node CLI things.
+
+  ${sym.warning} ${warning(` WARNING `)} Never do what I do.
+
+  ${sym.error} ${error(` ERROR `)} You did it now.
+`)
